984643eef43e2856313c359d043ed23c14ec8163
QinBenxue <qinbenxue@uniontech.com>
2022-12-27 11:40:00 +0800

    feat: update dsettingsdialog demo
    更新desettingsdialog示例
    
    Log:

--- DSettingsDialog/main.cpp
+++ DSettingsDialog/main.cpp
@@ -2,11 +2,15 @@
 #include <DMainWindow>
 #include <DSettingsDialog>
 #include <QTemporaryFile>
-#include <QDebug>
 #include <DPushButton>
 #include <QObject>
+#include <DSettingsWidgetFactory>
+#include <DSettingsOption>
+#include <DIconButton>
+#include "dtkcore_global.h"
 #include <qsettingbackend.h>
 DWIDGET_USE_NAMESPACE
+DCORE_USE_NAMESPACE
 
 int main(int argc, char *argv[]){
     DApplication app(argc, argv);
@@ -20,14 +24,20 @@ int main(int argc, char *argv[]){
         //初始化一个存储后端
         QTemporaryFile tmpFile;
         tmpFile.open();
-        Dtk::Core::QSettingBackend backend(tmpFile.fileName());
+        QSettingBackend backend(tmpFile.fileName());
 
         //从json中初始化配置
-        auto settings = Dtk::Core::DSettings::fromJsonFile(":/settingsData/settings2.json");
+        auto settings = DSettings::fromJsonFile(":/settingsData/settings.json");
         settings->setBackend(&backend);
 
         //初始化一个设置显示窗口
         DSettingsDialog dsd(&win);
+        //如果自带控件不能满足需求，可自行注册控件，此处以一个Icon button为例
+        dsd.widgetFactory()->registerWidget("icon-button", [&win](QObject *obj){
+            DSettingsOption *option = qobject_cast<DSettingsOption *>(obj);
+            DIconButton *iconBtn = new DIconButton(DStyle::SP_DriveCDIcon);//新建一个icon button
+            return iconBtn;
+        });
         dsd.updateSettings(settings); //将设置加载到设置窗口
         dsd.exec(); //设置窗口显示并等待响应
     });
